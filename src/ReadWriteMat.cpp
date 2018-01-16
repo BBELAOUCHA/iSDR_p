14459120d14b9585bbb8cc2d265f6cf1a6ca70b6
Brahim Belaoucha <BBELAOUCHA@users.noreply.github.com>
2018-01-16 20:19:49 +0100

    add Max Eig and MVAR to output file

--- src/ReadWriteMat.cpp
+++ src/ReadWriteMat.cpp
@@ -49,7 +49,6 @@ int ReadWriteMat::ReadData(const char *file_path, Maths::DMatrix &G_o,
             for (long unsigned int x = 0; x < n_c; ++x)
                 GA.data()[x + y*n_c] = xData[x + y*n_c]; 
         }
-
         Mat_VarFree(matvar);
         matvar = Mat_VarRead(matfp, "M") ;
         const double *xData1 = static_cast<const double*>(matvar->data);
@@ -72,7 +71,6 @@ int ReadWriteMat::ReadData(const char *file_path, Maths::DMatrix &G_o,
             for (long unsigned int x = 0; x < n_s; x++)
                 SC.data()[x*n_s + y] = (int)xData2[x*n_s+y];
         }
-
         xData_ = NULL;
         xData = NULL;
         xData1 = NULL;
@@ -88,10 +86,11 @@ int ReadWriteMat::ReadData(const char *file_path, Maths::DMatrix &G_o,
 }
 
 
-int ReadWriteMat::WriteData(const char *file_path, Maths::DMatrix &S,
-                Maths::DMatrix &mvar, Maths::IVector &A, Maths::DVector &w){
+int ReadWriteMat::WriteData(const char *file_path, Maths::DMatrix &S, Maths::DMatrix &mvar,
+                Maths::DMatrix &mvar_n, Maths::IVector &A, Maths::DVector &w, double max_eigenvalue){
     double mat1[n_s][n_t_s];
     double mat2[n_s*m_p][n_s];
+    double mat2_[n_s*m_p][n_s];
     double mat3[n_s];
     double mat4[n_s];
     unsigned int i, j;
@@ -99,10 +98,12 @@ int ReadWriteMat::WriteData(const char *file_path, Maths::DMatrix &S,
         for(i=0;i<n_t_s;i++)
             mat1[j][i] = S.data()[n_t_s*j+i];
     }
-    for(i=0;i<n_s * m_p;i++)
-        for(j=0;j<n_s;j++)
+    for(i=0;i<n_s * m_p;i++){
+        for(j=0;j<n_s;j++){
             mat2[i][j] = mvar.data()[j + n_s*i];
-
+            mat2_[i][j] = mvar_n.data()[j + n_s*i];
+        }
+    }
     for(j=0;j<n_s;j++){
       mat3[j] = A.data()[j];
       mat4[j] = w.data()[j];
@@ -113,29 +114,34 @@ int ReadWriteMat::WriteData(const char *file_path, Maths::DMatrix &S,
     size_t dims1[2] = {n_t_s,n_s};
     size_t dims2[2] = {n_s,n_s*m_p};
     size_t dim1d[1] = {n_s};
+    double sca1[1];
+    sca1[0] = max_eigenvalue;
+    size_t dims[1] = {(unsigned int)1};
     mat = Mat_Create(file_path,NULL);
     if(mat != NULL){
+        matvar = Mat_VarCreate("Eigen max", MAT_C_DOUBLE, MAT_T_DOUBLE,1, dims, &sca1,0);
+        Mat_VarWrite(mat, matvar, MAT_COMPRESSION_NONE);
+        Mat_VarFree(matvar);
         /* Estimated brain activation */
-        matvar = Mat_VarCreate("S estimate",MAT_C_DOUBLE,MAT_T_DOUBLE,2, dims1,
-            &mat1,0);
+        matvar = Mat_VarCreate("S estimate",MAT_C_DOUBLE,MAT_T_DOUBLE,2, dims1, &mat1,0);
         Mat_VarWrite( mat, matvar, MAT_COMPRESSION_NONE);
         Mat_VarFree(matvar);
         /* multivariate autoregresive model elements */
-        matvar = Mat_VarCreate("S MVAR", MAT_C_DOUBLE,MAT_T_DOUBLE,2, dims2,
-             &mat2,0);
+        matvar = Mat_VarCreate("MVAR", MAT_C_DOUBLE,MAT_T_DOUBLE,2, dims2, &mat2,0);
         Mat_VarWrite( mat, matvar, MAT_COMPRESSION_NONE);
         Mat_VarFree(matvar);
-      /* Label of active sources/regions */
-      matvar = Mat_VarCreate("S Active",MAT_C_DOUBLE,MAT_T_DOUBLE,1, dim1d,
-             &mat3,0);
-      Mat_VarWrite( mat, matvar, MAT_COMPRESSION_NONE);
-      Mat_VarFree(matvar);
-      /* weights used to normalize MVAR coeffitions */
-      matvar = Mat_VarCreate("Weights", MAT_C_DOUBLE, MAT_T_DOUBLE,1, dim1d,
-             &mat4,0);
-      Mat_VarWrite( mat, matvar, MAT_COMPRESSION_NONE);
-      Mat_VarFree(matvar);
-      Mat_Close(mat);
+        matvar = Mat_VarCreate("MVAR normalized", MAT_C_DOUBLE,MAT_T_DOUBLE,2, dims2, &mat2_,0);
+        Mat_VarWrite( mat, matvar, MAT_COMPRESSION_NONE);
+        Mat_VarFree(matvar);
+        /* Label of active sources/regions */
+        matvar = Mat_VarCreate("Active set",MAT_C_DOUBLE,MAT_T_DOUBLE,1, dim1d, &mat3,0);
+        Mat_VarWrite( mat, matvar, MAT_COMPRESSION_NONE);
+        Mat_VarFree(matvar);
+        /* weights used to normalize MVAR coeffitions */
+        matvar = Mat_VarCreate("Weights", MAT_C_DOUBLE, MAT_T_DOUBLE,1, dim1d, &mat4,0);
+        Mat_VarWrite( mat, matvar, MAT_COMPRESSION_NONE);
+        Mat_VarFree(matvar);
+        Mat_Close(mat);
     }
     else{
         printf("Failed to save results in %s", file_path);
