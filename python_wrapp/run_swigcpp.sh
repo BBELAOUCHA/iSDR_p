2e83d486d11c6d105228d2faf8cf7c5d2fddec98
bbelaoucha <brahim.belaoucha@gmail.com>
2019-11-05 18:51:55 +0100

    -

--- python_wrapp/run_swigcpp.sh
+++ python_wrapp/run_swigcpp.sh
@@ -15,3 +15,13 @@ gcc -shared -Wl,--no-as-needed PyiSDRcpp.o MxNE.o iSDR.o iSDR_modulecpp.o -O4 -W
 -I/home/bbelaouc/Wokspace/Project/iSDR_p/inc -DMKL_ILP64 -m64 -march=native -L${MKL_FOLDER}/mkl/lib/intel64_lin -L/usr/lib64/ \
 -L/home/bbelaouc/Wokspace/Needed/hdf5/hdf5/lib/ -L/home/bbelaouc/Wokspace/Needed/intel/compilers_and_libraries_2018.1.163/linux/compiler/lib/intel64_lin/ \
  -lmatio -lhdf5 -lmkl_intel_ilp64 -lmkl_gnu_thread -lmkl_core -lpthread -lm -ldl -lmkl_rt -liomp5 -lstdc++ -o _PyiSDRcpp.so
+
+
+or
+
+
+gcc -shared -Wl,--no-as-needed PyiSDRcpp.o MxNE.o iSDR.o iSDR_modulecpp.o -O4 -Wall -DWITH_MKLBLAS -DMKL_ILP64 -m64 -march=native \
+-I/usr/include -I${MKL_FOLDER}/mkl/include -I/home/bbelaouc/Wokspace/Needed/FLENS -I/usr/local/include/ \
+-I/home/bbelaouc/Wokspace/Project/iSDR_p/inc -DMKL_ILP64 -m64 -march=native -L${MKL_FOLDER}/mkl/lib/intel64_lin -L/usr/lib64/ \
+-L/home/bbelaouc/Wokspace/Needed/hdf5/hdf5/lib/ -L/home/bbelaouc/Wokspace/Needed/intel/compilers_and_libraries_2018.1.163/linux/compiler/lib/intel64_lin/ \
+ -lmatio -lhdf5 -lmkl_intel_ilp64 -lmkl_gnu_thread -lmkl_core -lpthread -lm -ldl -lmkl_rt -fopenmp -lstdc++ -o _PyiSDRcpp.so
